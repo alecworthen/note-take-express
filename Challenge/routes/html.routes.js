417b5795874142493891a9454477b4d7ecda7904
Alec Worthen <alecworthen@gmail.com>
2023-12-05 20:48:29 -0800

    updated html route

--- Challenge/routes/html.routes.js
+++ Challenge/routes/html.routes.js
@@ -2,13 +2,11 @@ const router = require('express').Router();
 const path = require('path');
 
 router.get('/', (req, res) => {
-    const absolutePath = path.join(__dirname, '../public/index.html');
-    res.sendFile(absolutePath);
+    res.sendFile(path.join(__dirname, '../public/index.html'));
 });
 
 router.get('/notes', (req, res) => {
-    const absolutePath = path.join(__dirname, '../public/notes.html');
-    res.sendFile(absolutePath);
+    res.sendFile(path.join(__dirname, '../public/notes.html'));
 });
 
 module.exports = router;
