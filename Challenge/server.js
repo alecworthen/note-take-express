c7d71884fc89bdd5b529d2ea49b2bbc86790bb91
Alec Worthen <alecworthen@gmail.com>
2023-12-05 21:41:30 -0800

    fixed notes page

--- Challenge/server.js
+++ Challenge/server.js
@@ -1,18 +1,17 @@
-const express = require('express');
-const htmlRoutes = require('./routes/html.routes');
-const apiRoutes = require('./routes/api-routes');
+const express = require("express");
+const htmlRoutes = require("./routes/html.routes");
+const apiRoutes = require("./routes/api-routes");
 const PORT = process.env.PORT || 3001;
 
 const app = express();
 
-app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static("public"));
 
-app.use(express.static('public'));
-
-app.use(htmlRoutes);
 app.use(apiRoutes);
+app.use(htmlRoutes);
 
 app.listen(PORT, () => {
-    console.log(`Server running on http://localhost:${PORT}`);
+  console.log(`Server running on http://localhost:${PORT}`);
 });
